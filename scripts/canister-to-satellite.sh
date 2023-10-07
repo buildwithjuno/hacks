538c50b97c020a0705d348451920f8b054e3644e
David Dal Busco <david.dalbusco@outlook.com>
2023-10-07 20:50:31 +0200

    chore: repo transferred to new org junobuild

--- scripts/canister-to-satellite.sh
+++ scripts/canister-to-satellite.sh
@@ -11,6 +11,6 @@ DFX_ID=$(dfx identity get-principal)
 JUNO_VERSION=v0.0.16
 SATELLITE_VERSION=v0.0.11
 
-curl -L -o satellite.wasm.gz "https://github.com/buildwithjuno/juno/releases/download/${JUNO_VERSION}/satellite-${SATELLITE_VERSION}.wasm.gz"
+curl -L -o satellite.wasm.gz "https://github.com/junobuild/juno/releases/download/${JUNO_VERSION}/satellite-${SATELLITE_VERSION}.wasm.gz"
 
 dfx canister install ${CANISTER_ID} --argument '(record { controllers = vec { principal"'${MISSION_CONTROL_ID}'"; principal"'${DEVELOPER_ID}'"; principal"'${CLI_ID}'"; principal"'${DFX_ID}'";} })' --mode reinstall
